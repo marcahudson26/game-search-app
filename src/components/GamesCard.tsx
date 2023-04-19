27f124e132835c8e3af620bb4d05b1a992d9a9f3
marc hudson <marchudson2601@gmail.com>
2023-04-19 14:03:05 +0100

    added platform icons

--- src/components/GamesCard.tsx
+++ src/components/GamesCard.tsx
@@ -1,16 +1,20 @@
-import React from "react";
-import { Card, Image, CardBody, Heading } from "@chakra-ui/react";
+import { Card, Image, CardBody, Heading, Text } from "@chakra-ui/react";
 import { Games } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
+// import PlatformIconList from "./PlatformIconList";
 
 interface Props {
   game: Games;
 }
 const GamesCard = ({ game }: Props) => {
   return (
-    <Card>
+    <Card borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
-        <Heading>{game.name}</Heading>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
