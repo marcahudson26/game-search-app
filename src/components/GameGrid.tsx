d7a4748af4d0bf229e70f72c62a6594102991c5f
marc hudson <marchudson2601@gmail.com>
2023-04-19 15:00:08 +0100

    added card skeleton onload

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Skeleton, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GamesCard from "./GamesCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+
+  const Skeletons = [1, 2, 3, 4, 5, 6, 7];
   return (
     <>
       {error && <Text>{error}</Text>}
@@ -13,6 +16,8 @@ const GameGrid = () => {
         padding="10px"
         spacing={10}
       >
+        {isLoading &&
+          Skeletons.map((Skeleton) => <GameCardSkeleton key={Skeleton} />)}
         {games.map((game) => (
           <GamesCard key={game.id} game={game} />
         ))}
